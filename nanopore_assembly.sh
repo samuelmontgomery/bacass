d005f05a09029b931aee8404ddf9c74915f279e3
samuelmontgomery <51734392+samuelmontgomery@users.noreply.github.com>
2024-01-24 12:15:53 +0800

    Update nanopore_assembly.sh
    Simplified the functions for QC, and made annotation optional via cmd --annotate

--- nanopore_assembly.sh
+++ nanopore_assembly.sh
@@ -1,90 +1,74 @@
 #!/bin/bash
 
 # Check if the directory path argument is provided
-if [ $# -ne 1 ]; then
+if [[ $# -lt 1 ]]; then
   echo "Please provide the directory path as a command-line argument."
   exit 1
 fi
 
 # Get the directory path from the command-line argument
-export directory="$1"
+directory="${1}"
 
 # Check if the specified directory exists
-if [ ! -d "$directory" ]; then
-  echo "Directory does not exist: $directory"
+if [[ ! -d "${directory}" ]]; then
+  echo "Directory does not exist: ${directory}"
   exit 1
 fi
 
 # Get a list of all immediate subfolders in the specified directory
-folders=($(find "$directory" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
+folders=($(find "${directory}" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
+
+# Check if --annotate is passed as an argument
+annotate=false
+if [[ $# -ge 2 ]]; then
+  if [[ "${2}" == "--annotate" ]]; then
+    annotate=true
+  fi
+fi
 
 # Function to process each folder for initial steps (2 cores per job)
 process_initial_steps() {
-  folder="$1"
-  echo "Processing folder: $folder"
-
-  # Navigate to the folder
-  cd "$directory/$folder"
+  folder="${1}"
+  echo "Processing folder: ${folder}"
 
   # Concatenate all fastq files into a single file
-  cat *.fastq.gz > "$folder.fastq.gz"
+  cat "${directory}/${folder}"/*.fastq.gz > "${directory}/${folder}/${folder}.fastq.gz"
 
   # Create output dir variable
-  mkdir reads_qc
-
-  # Remove DNA CS from data
-  gunzip -c "$folder.fastq.gz" | chopper -l 4000 --contam /home/ubuntu/scratch/references/dna_cs.fasta | gzip > "$folder"_cs.fastq.gz
-
-  # Filter reads using filtlong
-  filtlong --keep_percent 95 "$folder"_cs.fastq.gz | gzip > reads_qc/"$folder"_filtered.fastq.gz
+  mkdir "${directory}/${folder}/reads_qc"
   
-  # Return to the original directory
-  cd -
+  # Filter reads using filtlong
+  filtlong --minlength 4000 --keep_percent 95 --target_bases 700000000 --verbose "${directory}/${folder}/${folder}.fastq" | gzip > "${directory}/${folder}/reads_qc/${folder}_filtered.fastq.gz"
 }
 
+# Function to process each folder for QC steps
 process_qc() {
-  folder="$1"
-  echo "Running NanoPlot: $folder"
-
-  # Navigate to the folder
-  cd "$directory/$folder"
+  folder="${1}"
+  echo "Running NanoPlot: ${folder}"
 
   # Create NanoPlot QC plots
-  NanoPlot -t 2 --huge -o ./nanoplot --N50 --tsv_stats --loglength --info_in_report --fastq_rich reads_qc/"$folder"_filtered.fastq.gz
-
-  # Return to the original directory
-  cd -
+  NanoPlot -t 2 --huge -o "${directory}/${folder}/nanoplot" --N50 --tsv_stats --loglength --info_in_report --fastq_rich "${directory}/${folder}/reads_qc/${folder}_filtered.fastq.gz"
 }
 
+# Function to process each folder for assembly steps
 process_assembly() {
-  folder="$1"
-  echo "Running NanoPlot: $folder"
+  folder="${1}"
+  echo "Running assembly: ${folder}"
 
-  # Navigate to the folder
-  cd "$directory/$folder"
-  
   # Assemble using flye and polish with mekada
-  flye --nano-hq reads_qc/"$folder"_filtered.fastq.gz --asm-coverage 50 --scaffold --meta --out-dir flye --threads 8
-
-  # Return to the original directory
-  cd -
+  flye --nano-hq "${directory}/${folder}/reads_qc/${folder}_filtered.fastq.gz" --asm-coverage 50 --scaffold --meta --out-dir "${directory}/${folder}/flye" --threads 8
 }
 
+# Function to process each folder for annotation steps
 process_annotate() {
-  folder="$1"
-  echo "Running bakta: $folder"
+  folder="${1}"
+  echo "Running bakta: ${folder}"
 
-  # Navigate to the folder
-  cd "$directory/$folder"
-  
   # Annotate with bakta
-  bakta flye/assembly.fasta --output bakta --verbose --debug --complete --threads 4
+  bakta "${directory}/${folder}/flye/assembly.fasta" --output "${directory}/${folder}/bakta" --verbose --debug --complete --threads 4
 
   # Run amrfinder
-  amrfinder -p bakta/assembly.faa -g bakta/assembly.gff3 -n bakta/assembly.fna -a bakta --organism Pseudomonas_aeruginosa -d /home/ubuntu/scratch/references/bakta/db/amrfinderplus-db/latest --threads 4 --plus -o amr.txt
-
-  # Return to the original directory
-  cd -
+  amrfinder -p "${directory}/${folder}/bakta/assembly.faa" -g "${directory}/${folder}/bakta/assembly.gff3" -n "${directory}/${folder}/bakta/assembly.fna" -a "${directory}/${folder}/bakta" --organism Pseudomonas_aeruginosa -d /home/ubuntu/scratch/references/bakta/db/amrfinderplus-db/latest --threads 4 --plus -o "${directory}/${folder}/bakta/amr.txt"
 }
 
 export -f process_initial_steps
@@ -95,29 +79,33 @@ export -f process_annotate
 parallel -j 8 --eta -k process_initial_steps ::: "${folders[@]}"
 parallel -j 8 --eta -k process_qc ::: "${folders[@]}"
 parallel -j 2 --eta -k process_assembly ::: "${folders[@]}"
-parallel -j 4 --eta -k process_annotate ::: "${folders[@]}"
 
-mkdir $directory/checkm
+# Check if annotation should be run
+if [[ "${annotate}" == true ]]; then
+  parallel -j 4 --eta -k process_annotate ::: "${folders[@]}"
+fi
 
+# Create checkm directory
+mkdir ${directory}/checkm
+
+# Copy assemblies to the checkm file
 for folder in "${folders[@]}"
 do
-  if [ "$folder" != "checkm" ]; then
-    # Navigate to the folder
-    cd "$directory/$folder"
-  
-    # Assemble using flye and polish with mekada
-    cp bakta/assembly.fna "$directory/checkm"
-    mv "$directory/checkm/assembly.fna" "$directory/checkm/${folder}_assembly.fna"
-  
-    # Return to the original directory
-    cd -
+  if [[ "${folder}" != "checkm" ]]; then
+    # Check if annotation was run
+    if [[ "${annotate}" == true ]]; then
+      # Move assembly to checkm folder, renaming the file with the folder name
+      cp "${directory}/${folder}/bakta/assembly.fna" "${directory}/checkm/${folder}_assembly.fna"
+    else
+      cp "${directory}/${folder}/flye/assembly.fasta" "${directory}/checkm/${folder}_assembly.fasta"
+    fi
   fi
 done
 
 # Run CheckM2 on all genomes
 conda activate checkm
 
-checkm2 predict --threads 16 --input $directory/checkm --output-directory $directory/checkm/checkm --tmpdir /home/ubuntu/scratch/tmp
+checkm2 predict --threads 16 --input ${directory}/checkm --output-directory ${directory}/checkm/output --tmpdir /home/ubuntu/scratch/tmp
 
 conda activate nano
 
