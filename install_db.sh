a35d73253dacd6077910aaf0eac8cc1b295f827d
Samuel Montgomery <samuel.montgomery@gmail.com>
2025-06-09 06:59:36 +0000

    amend database install scripts

--- install_db.sh
+++ install_db.sh
@@ -3,7 +3,7 @@
 #help menu function
 help() {
     cat << EOF
-Usage: $(basename "$0") -d DATABASE -c [docker|singularity]
+Usage: $(basename "$0") -d DATABASE
 
 Options:
   -d, --database    Specify the directory for database installation (required)
@@ -22,12 +22,18 @@ done
 
 # Create necessary database directories
 mkdir -p "${database}/bakta" \
-    && mkdir "${database}/kraken2" \
-    && mkdir "${database}/checkm"
+    && mkdir "${database}/checkm" \
+    && mkdir "${database}/checkm2"
 
 # Run filtering steps via docker image 
 docker run \
     -v "${database}":/workdir/database \
     -u `id -u $USER`:`id -g $USER` \
-    samueltmontgomery/bacassdb \
+    samueltmontgomery/bacass \
     /workdir/bin/db_install.sh
+
+docker run \
+    -v "${database}":/workdir/database \
+    -u `id -u $USER`:`id -g $USER` \
+    samueltmontgomery/bacassqc \
+    /workdir/bin/qc_db_install.sh
--- lib/db_install.sh
+++ lib/db_install.sh
@@ -9,6 +9,11 @@ wget https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.t
 tar -xzvf checkm_data_2015_01_16.tar.gz -C "${database}"/checkm
 rm checkm_data_2015_01_16.tar.gz
 
+# Install Kraken2 database - standard 16GB
+wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_16gb_20250402.tar.gz
+tar -xzvf k2_standard_16gb_20250402.tar.gz -C "${database}"/kraken2
+rm k2_standard_16gb_20250402.tar.gz
+
 # Install Bakta database - full
 bakta_db download --output "${database}/bakta" --type full
 
